221f9be0a1f0002ff8d166d21c43fde25cdf33ac
rogeriosantos <rogerio.tuba@gmail.com>
2021-07-04 22:05:58 -0300

    Insercao HTML

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+# função post_list que leva a solicitação e irá 
+#retornar o valor que recebe ao chamar outra função render 
+#que irá renderizar (montar) nosso modelo blog/post_list.html
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
